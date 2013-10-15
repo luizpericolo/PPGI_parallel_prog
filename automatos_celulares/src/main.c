b6b35997507118307a4ebe8bffe909edc14f4e40
Luiz Pericolo <luizpericolo@gmail.com>
2013-10-15 20:39:48 -0300

    Giving the window a title :)

--- automatos_celulares/src/main.c
+++ automatos_celulares/src/main.c
@@ -28,6 +28,7 @@ int main(void)
     SDL_Event event;
 
     screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+    SDL_WM_SetCaption("Cellular Automata", NULL);
     
     //While the user hasn't quit
     while( quit == false )
