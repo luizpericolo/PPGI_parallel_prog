b4f29cd5af69bb9d3b0759ce76d2bef849a9c4a2
Luiz Pericolo <luizpericolo@gmail.com>
2013-10-15 20:40:57 -0300

    Removing unused functions and adding draw_grid.

--- automatos_celulares/src/cellular_automata_gfx.c
+++ automatos_celulares/src/cellular_automata_gfx.c
@@ -1,9 +1,10 @@
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 
+#include "cellular_automata.h"
 #include "cellular_automata_gfx.h"
 
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination)
 {
     //Temporary rectangle to hold the offsets
     SDL_Rect offset;
@@ -14,33 +15,6 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination 
     
     //Blit the surface
     SDL_BlitSurface(source, NULL, destination, &offset);
-}
-
-int init(SDL_Surface *screen)
-{
-	int ret = 0;
-    //Initialize all SDL subsystems
-	ret = SDL_Init(SDL_INIT_EVERYTHING);
-
-    if(ret == -1)
-    {
-        return false;    
-    }
-    
-    //Set up the screen
-    screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
-    
-    //If there was an error in setting up the screen
-    if(screen == NULL)
-    {
-        return false;    
-    }
-    
-    //Set the window caption
-    SDL_WM_SetCaption("Cellular Automata", NULL);
-    
-    //If everything initialized fine
-    return true;
 }
 
 
@@ -54,59 +28,64 @@ void clean_up(SDL_Surface *image)
 }
 
 
-int show_screen()
+SDL_Surface* draw_grid(int* grid, SDL_Surface *screen)
 {
+    /* Draws the grid passed as a parameter and fills the borders afterwards. */
+    SDL_Surface *cell;
 
-	//The surfaces
-	SDL_Surface *image = NULL;
-	SDL_Surface *screen = NULL;
+    cell = SDL_LoadBMP("img/cell.bmp");
 
-	int x = 0;
-	int y = 0;
+    int x, y;
+    int i;
 
-
-    //Make sure the program waits for a quit
-    int quit = false;
-
-    int status = true;
-
-    //The event structure that will be used
-	SDL_Event event;
-
-    
-   	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
-    image = SDL_LoadBMP("sdl_test.bmp");
-    SDL_BlitSurface(image, NULL, screen, NULL);
-    
-    //Apply the surface to the screen
-    apply_surface(x, y, image, screen);
-    
-    //Update the Screen
-    SDL_Flip(screen);
-    
-    //While the user hasn't quit
-    while( quit == false )
+    for(i=0; i<CELL_COUNT; i++)
     {
-    	SDL_FillRect(screen , NULL , 0x221122);
-    	x += 1;
-    	y += 1;
-    	apply_surface(x, y, image, screen);
-    	SDL_Flip(screen);
-
-    	  //While there's an event to handle
-        while( SDL_PollEvent( &event ) )
+        if(grid[i] == 1)
         {
-        	 //If the user has Xed out the window
-            if( event.type == SDL_QUIT )
-            {
-                //Quit the program
-                quit = true;
-            }    
+
+            x = ((i % GRID_WIDTH) * 16) + 1;
+            y = ((i / GRID_WIDTH) * 16) + 1;
+
+            apply_surface(x, y, cell, screen);
         }
+
+        // Adding the top border.
+        if(i / GRID_WIDTH == 0)
+        {
+            x = ((i % GRID_WIDTH) * 16) + 1;
+            y = ((i / GRID_WIDTH) * 16) + 1;
+
+            apply_surface(x, y, cell, screen);
+        }
+
+        // Adding the left border.
+        if(i % GRID_WIDTH == 0)
+        {
+            x = ((i % GRID_WIDTH) * 16) + 1;
+            y = ((i / GRID_WIDTH) * 16) + 1;
+
+            apply_surface(x, y, cell, screen);
+        }
+
+        // Adding the right border.
+        if(i % GRID_WIDTH == GRID_WIDTH - 1)
+        {
+            x = ((i % GRID_WIDTH) * 16) + 1;
+            y = ((i / GRID_WIDTH) * 16) + 1;
+
+            apply_surface(x, y, cell, screen);
+        }
+
+        // Adding the bottom border.
+        if(i / GRID_WIDTH == GRID_WIDTH - 1)
+        {
+            x = ((i % GRID_WIDTH) * 16) + 1;
+            y = ((i / GRID_WIDTH) * 16) + 1;
+
+            apply_surface(x, y, cell, screen);
+        }
+        
     }
 
-     //Free the surface and quit SDL
-    clean_up(image);
-        
-    return 0;    
+    return screen;
 }
