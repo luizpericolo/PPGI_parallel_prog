7886fbc81bde7723b10166e4bcb06afe3025a75f
Luiz Pericolo <luizpericolo@gmail.com>
2013-10-15 20:32:11 -0300

    Headers for new functions and new constant FILL_PROB used when generating a random initial population.

--- automatos_celulares/src/cellular_automata.h
+++ automatos_celulares/src/cellular_automata.h
@@ -1,9 +1,12 @@
 // Defining some constants
-#define GRID_WIDTH 5
-#define GRID_HEIGHT 5
+#define GRID_WIDTH 30
+#define GRID_HEIGHT 30
 #define CELL_COUNT GRID_WIDTH * GRID_HEIGHT
+#define FILL_PROB 45
 
 
 int* create_random_initial_population();
 void print_grid(int *grid);
 int count_neighbors(int *grid, int x);
+int * apply_cave_generation_rule(int *grid);
+int * apply_4_5_rule(int *grid);
