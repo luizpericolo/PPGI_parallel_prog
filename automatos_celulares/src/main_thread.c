d0bd00438f4259ac944cf772c3749334c9efbadc
Luiz Pericolo <luizpericolo@gmail.com>
2013-12-02 22:36:49 -0200

    Putting all functions in same file for thread version.

--- automatos_celulares/src/main_thread.c
+++ automatos_celulares/src/main_thread.c
@@ -2,27 +2,48 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define N_THREADS 1
+#define N_THREADS 8
 
-#define N_ITER 5
+#define N_ITER 500
 
-#define GRID_WIDTH 8
-#define GRID_HEIGHT 8
+#define GRID_WIDTH 1024
+#define GRID_HEIGHT 512
 #define CELL_COUNT GRID_WIDTH * GRID_HEIGHT
 #define FILL_PROB 45
 
+#include <sys/time.h>
+#include <time.h>
+
+typedef union{
+    unsigned long long int64;
+    struct{
+        unsigned int lo, hi;
+    } int32;
+} tsc_counter;
+
+#define RDTSC(cpu_c)             \
+__asm__ __volatile__("rdtsc" : \
+    "=a" ((cpu_c).int32.lo),  \
+    "=d"((cpu_c).int32.hi) )
+
 typedef struct pthread_params {
 	int id;
+	/*
 	int *born;
 	int n_born;
 	int *survive;
 	int n_survive;
+	*/
 } pthread_params;
 
 void print_grid(int *grid);
-void *PrintSomething(void *threadid);
+void simple_print_grid(int *grid);
+void *transition_cells(void *thread_params);
+int count_neighbors(int *grid, int x);
 int* create_random_initial_population();
 void print_name(int tid);
+void apply_cave_generation_rule(int *grid, int startPos, int endPos);
+void fill_borders(int *grid);
 
 int *current_gen;
 int *next_gen;
@@ -30,6 +51,117 @@ int *next_gen;
 void print_name(int tid)
 {
 	printf("Hi! I'm a thread and my name is %d\n", tid);
+}
+
+int count_neighbors(int *grid, int x)
+{
+    /* This functions counts how many neighbors equal to 1 a cell has (in all 8 directions!). */
+
+    int count = 0;
+
+    // Checking for the East Neighbor.
+    if(x % GRID_WIDTH != GRID_WIDTH - 1)
+    {
+        
+        if(grid[x + 1] == 1)
+        {
+            count++;
+        }
+    }
+
+    // Checking for the West Neighbor.
+    if(x % GRID_WIDTH != 0)
+    {
+        
+        if(grid[x - 1] == 1)
+        {
+            count++;
+        }
+    }
+
+    // Checking for the North Neighbor.
+    if(x - GRID_WIDTH >= 0)
+    {
+        
+        if(grid[x - GRID_WIDTH] == 1)
+        {
+            count++;
+        }
+
+        // Checking for the Northwest Neighbor.
+        if((x - GRID_WIDTH - 1) % GRID_WIDTH != GRID_WIDTH - 1  && x - GRID_WIDTH - 1 >= 0)
+        {
+            
+            if(grid[x - GRID_WIDTH - 1] == 1)
+            {
+                count++;
+            }
+                
+        }
+
+        // Checking for the Northeast Neighbor.
+        if((x - GRID_WIDTH + 1) % GRID_WIDTH != 0)
+        {
+            
+            if(grid[x - GRID_WIDTH + 1] == 1)
+            {
+                count++;
+            }
+                
+        }
+    }
+
+    // Checking for the South Neighbor.
+    if(x + GRID_WIDTH < CELL_COUNT)
+    {
+        
+        if(grid[x + GRID_WIDTH] == 1)
+        {
+            count++;
+        }
+            
+
+        // Checking for the Southwest Neighbor.
+        if((x + GRID_WIDTH - 1) % GRID_WIDTH != GRID_WIDTH - 1)
+        {
+            
+            if(grid[x + GRID_WIDTH - 1])
+            {
+                count++;
+            }
+                
+        }
+
+        // Checking for the Southeast Neighbor.
+        if((x + GRID_WIDTH + 1) % GRID_WIDTH != 0 && x + GRID_WIDTH + 1 < CELL_COUNT)
+        {
+            
+            if(grid[x + GRID_WIDTH + 1])
+            {
+                count++;
+            }
+                
+        }
+    }
+    return count;
+}
+
+void fill_borders(int *grid)
+{
+	int i;
+	int x, y;
+
+	for(i=0; i < CELL_COUNT; i++)
+	{
+		x = i % GRID_WIDTH;
+        y = i / GRID_WIDTH;
+
+        if(x == 0 || x == GRID_WIDTH-1)
+			grid[i] = 1;
+
+		if(y == 0 || y == GRID_HEIGHT-1)
+			grid[i] = 1;
+	}
 }
 
 void *transition_cells(void *thread_params)
@@ -41,20 +173,42 @@ void *transition_cells(void *thread_params)
 	int start = n_elements * (params->id);
 	int end = start + n_elements;
 
-	print_name(params->id);
-	int i;
-
-	next_gen = current_gen;
-
-	for(i=start; i<end; i++)
-	{
-	}
-
-	printf("--Thread %d terminou.\n", params->id);
+	apply_cave_generation_rule(current_gen, start, end);
 	
 	free(params);
 
 	pthread_exit(NULL);
+}
+
+void apply_cave_generation_rule(int *grid, int startPos, int endPos)
+{
+
+    int i;
+
+    int n_count = 0;
+
+    for(i=startPos; i<endPos; i++)
+    {
+        n_count = count_neighbors(grid, i);
+
+        // Applying the B678 rule.
+        if(grid[i] == 0)
+        {
+            if((n_count == 6) || (n_count == 7) || (n_count == 8))
+                next_gen[i] = 1;
+            else
+                next_gen[i] = grid[i];
+        }
+
+        // Applying the S345678
+        if(grid[i] == 1)
+        {
+            if((n_count == 0) || (n_count == 1) || (n_count == 2))
+                next_gen[i] = 0;
+            else
+                next_gen[i] = grid[i];
+        }
+    }
 }
 
 void print_grid(int *grid)
@@ -62,11 +216,14 @@ void print_grid(int *grid)
     /* This function iterates over the grid and prints it in a matricial fashion. */
     
     int i, j;
-    for(i=0; i<GRID_WIDTH; i++)
+    for(i=0; i<GRID_HEIGHT; i++)
     {
-        for(j=0;j<GRID_HEIGHT;j++)
+        for(j=0;j<GRID_WIDTH;j++)
         {
-            printf("%d", grid[(i * GRID_WIDTH) + j]);
+        	if(grid[(i * GRID_WIDTH) + j] == 1)
+            	printf("@");
+           	else
+           		printf(" ");
         }
 
         printf("\n");
@@ -74,6 +231,18 @@ void print_grid(int *grid)
 
     printf("\n");
 
+}
+
+void simple_print_grid(int *grid)
+{
+    /* This function iterates over the grid and prints it in a vectorial fashion. */
+    int i;
+
+    for(i=0; i<CELL_COUNT; i++)
+    {
+    	printf("%d", grid[i]);
+    }
+    printf("\n");
 }
 
 int* create_random_initial_population()
@@ -100,6 +269,7 @@ int* create_random_initial_population()
         }
             
     }
+
     return grid;
 }
 
@@ -114,20 +284,29 @@ int main(void)
 	pthread_t tid[N_THREADS];
 	int t;
 
+	struct timeval inicio, fim;
+    tsc_counter tsc1, tsc2;
+    long long unsigned int clock;
+    double tempo;
+
+
+    gettimeofday(&inicio, NULL);
+    RDTSC(tsc1); 
+
 	current_gen = create_random_initial_population();
 	next_gen = malloc(CELL_COUNT * sizeof(int));
-	printf("First Generation: \n");
-	print_grid(current_gen);
+	fill_borders(current_gen);
+	//print_grid(current_gen);	
 
 	int n;
 	for(n=0; n<N_ITER; n++)
 	{
-
-		for(t=0; t<N_THREADS; t++)
+        for(t=0; t<N_THREADS; t++)
 		{
 			pthread_params *params;
 			params = malloc(sizeof(pthread_params));
 			params->id = t;
+			/*
 			//B678
 			params->n_born = 3;
 			params->born = malloc(3 * sizeof(int));
@@ -144,6 +323,7 @@ int main(void)
 			params->survive[3] = 6;
 			params->survive[4] = 7;
 			params->survive[5] = 8;
+			*/
 			
 			pthread_create(&tid[t], NULL, transition_cells, (void*) params);
 		}
@@ -153,11 +333,23 @@ int main(void)
 			pthread_join(tid[t], NULL);
 		}
 
-		// Using the last iteration's result as input for the next iteration.
 		current_gen = next_gen;
+		fill_borders(current_gen);
 
-		print_grid(current_gen);
+		//print_grid(current_gen);
 	}
+
+	RDTSC(tsc2);
+	gettimeofday(&fim, NULL);
+
+ 	tempo = (fim.tv_sec - inicio.tv_sec) * 1000 + (fim.tv_sec - inicio.tv_sec)/1000;
+    printf("Tempo: %.2lf\n", tempo);
+
+    clock = tsc2.int64 - tsc1.int64;
+    printf("Tempo: %.2lf(ms) Clocks: %.2e\n", tempo/N_ITER, (double)clock/N_ITER);
+    printf("Clock/tempo: %.2e\n\n", clock/tempo);
+
+    print_grid(current_gen);
 
 	pthread_exit(NULL);
 }
