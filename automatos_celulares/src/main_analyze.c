06a2749cf0a638b881b11797d790eb53e212ca7a
Luiz Pericolo <luizpericolo@gmail.com>
2013-12-15 23:59:20 -0200

    Getting arguments through argc and argv instead of using #define.

--- automatos_celulares/src/main_analyze.c
+++ automatos_celulares/src/main_analyze.c
@@ -5,7 +5,7 @@
 //#include<SDL/SDL.h>
 
 // Including our lib-ish source headers.
-#include "cellular_automata.h"
+//#include "cellular_automata.h"
 //#include "cellular_automata_gfx.h"
 
 #include <stdio.h>
@@ -23,7 +23,15 @@ typedef union{
 #define RDTSC(cpu_c)             \
 __asm__ __volatile__("rdtsc" : \
     "=a" ((cpu_c).int32.lo),  \
-    "=d"((cpu_c).int32.hi) )
+    "=d"((cpu_c).int32.hi) );
+
+int N_ITER;
+int N_SIM;
+int N_MAPS;
+int GRID_WIDTH;
+int GRID_HEIGHT;
+int CELL_COUNT;
+int FILL_PROB;
 
 int* create_random_initial_population()
 {
@@ -199,56 +207,83 @@ int * apply_cave_generation_rule(int *grid)
                 next_gen[i] = grid[i];
         }
     }
-
+    free(grid);
     return next_gen;
 }
-int main(void)
+
+int main(int argc, char * argv[])
 {
+
+    if(argc < 6)
+    {
+    printf("[Error]\n");
+        printf("Useage: %s <n_sim> <n_maps> <grid_width> <grid_height> <fill_prob>\n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        N_SIM = atoi(argv[1]);          //5;
+        N_MAPS = atoi(argv[2]);         //500;
+        GRID_WIDTH = atoi(argv[3]);     //256;
+        GRID_HEIGHT = atoi(argv[4]);    //64;
+        FILL_PROB = atoi(argv[5]);      //45;    
+
+
+        N_ITER = 5;
+        CELL_COUNT = GRID_WIDTH * GRID_HEIGHT;
+    }
 
     struct timeval inicio, fim;
     tsc_counter tsc1, tsc2;
     long long unsigned int clock;
-    double tempo;
+    double tempo, tempo_total;
 
     int *grid;
     //print_grid(grid);
 
-    int i;
-    int n;
+    int i,s;
 
-    gettimeofday(&inicio, NULL);
-    RDTSC(tsc1); 
-
-    grid = create_random_initial_population(grid);
-
-    for(i=0; i<N_ITER;i++)
+    for(s=0; s<N_SIM; s++)
     {
-        
-        // Creating the random initial population for the Cellular Automata.
+        gettimeofday(&inicio, NULL);
+        RDTSC(tsc1); 
 
-        //for(n=0; n<5; n++)
-        //{
-            grid = apply_cave_generation_rule(grid);
+        int m;
+
+        for(m=0; m<N_MAPS; m++)
+        {
+            grid = create_random_initial_population(grid);
+
+            for(i=0; i<N_ITER;i++)
+            {
+                grid = apply_cave_generation_rule(grid);
+                //print_grid(grid);
+            }
             //print_grid(grid);
-        //}
+        }
 
-    
+        RDTSC(tsc2);
+        gettimeofday(&fim, NULL);
+
+        printf("Run #%d\n", s);
+        tempo = (fim.tv_sec - inicio.tv_sec) * 1000 + (fim.tv_sec - inicio.tv_sec)/1000;
+        printf("Tempo: %.2lf\n", tempo);
+
+        clock = tsc2.int64 - tsc1.int64;
+        printf("Tempo: %.2lf(ms) Clocks: %.2e\n", tempo/N_ITER, (double)clock/N_ITER);
+        printf("Clock/tempo: %.2e\n", clock/tempo);
+
+        tempo_total += tempo;
+
+        free(grid);
+
     }
+    printf("\nResultados: \n");
+    printf("Tempo Total: %.2lf\n", tempo_total);
+    printf("Tempo Médio: %.2lf\n", (double) tempo_total/N_SIM);
 
-    RDTSC(tsc2);
-    gettimeofday(&fim, NULL);
-
-    tempo = (fim.tv_sec - inicio.tv_sec) * 1000 + (fim.tv_sec - inicio.tv_sec)/1000;
-    printf("Tempo: %.2lf\n", tempo);
-
-    clock = tsc2.int64 - tsc1.int64;
-    printf("Tempo: %.2lf(ms) Clocks: %.2e\n", tempo/N_ITER, (double)clock/N_ITER);
-    printf("Clock/tempo: %.2e\n\n", clock/tempo);
-
-    print_grid(grid);
-        
-    
     return 0;
 }
+
 
 
